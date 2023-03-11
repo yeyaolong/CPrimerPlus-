19a4672756d822e0191a8479746d590ec635aebb
叶瑶隆 <yaolong.ye@dbappsecurity.com.cn>
2023-03-11 17:45:59 +0800

    创建项目

---
+++ 第二章开始学习CPrimerPlus/myfirst.cpp
@@ -0,0 +1,10 @@
+#include<iostream> // a PREPROCESSOR directive
+
+int main() {
+	using namespace std;	// make definitions visible
+	cout << "Come up and C++ me soem time.";
+	cout << endl;
+	cout << "You won't regret it!" << endl;
+
+	return 0;	// terminate main()
+}	// end of function body
