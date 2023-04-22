6023096c7bded3c4b6663874f4e7a4ee973d02f2
叶瑶隆 <yaolong.ye@dbappsecurity.com.cn>
2023-04-22 14:22:35 +0800

    暂存

---
+++ 第5章循环和关系表达式/forloop.cpp
@@ -0,0 +1,14 @@
+// ≥Ã–Ú«Âµ•5.1 forloop.cpp -- introducing the for loop
+#include<iostream>
+int main1()
+{
+	using namespace std;
+	int i; // create a counter
+	// initialize; test; update
+	for (i = 0; i < 5; i++) {
+		cout << "C++ knows loops.\n";
+	}
+	cout << "C++ knows when to stop.\n";
+
+	return 0;
+}
